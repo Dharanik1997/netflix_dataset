fedec8314e192f29d52c8a097dd352f7b102496e
Dharanik1997 <kondlapudidharanireddy@gmail.com>
2025-05-15 13:44:12 -0500

    Update MySQL Queries.sql

--- MySQL Queries.sql
+++ MySQL Queries.sql
@@ -1,138 +1,193 @@
--- Walmart Project Queries - MySQL
+-- Netflix Data Analysis using SQL
+-- Solutions of 15 business problems
+-- 1. Count the number of Movies vs TV Shows
 
-SELECT * FROM walmart;
-
--- DROP TABLE walmart;
-
--- DROP TABLE walmart;
-
--- Count total records
-SELECT COUNT(*) FROM walmart;
-
--- Count payment methods and number of transactions by payment method
 SELECT 
-    payment_method,
-    COUNT(*) AS no_payments
-FROM walmart
-GROUP BY payment_method;
+	type,
+	COUNT(*)
+FROM netflix
+GROUP BY 1
 
--- Count distinct branches
-SELECT COUNT(DISTINCT branch) FROM walmart;
+-- 2. Find the most common rating for movies and TV shows
 
--- Find the minimum quantity sold
-SELECT MIN(quantity) FROM walmart;
-
--- Business Problem Q1: Find different payment methods, number of transactions, and quantity sold by payment method
-SELECT 
-    payment_method,
-    COUNT(*) AS no_payments,
-    SUM(quantity) AS no_qty_sold
-FROM walmart
-GROUP BY payment_method;
-
--- Project Question #2: Identify the highest-rated category in each branch
--- Display the branch, category, and avg rating
-SELECT branch, category, avg_rating
-FROM (
+WITH RatingCounts AS (
     SELECT 
-        branch,
-        category,
-        AVG(rating) AS avg_rating,
-        RANK() OVER(PARTITION BY branch ORDER BY AVG(rating) DESC) AS rank
-    FROM walmart
-    GROUP BY branch, category
-) AS ranked
-WHERE rank = 1;
-
--- Q3: Identify the busiest day for each branch based on the number of transactions
-SELECT branch, day_name, no_transactions
-FROM (
-    SELECT 
-        branch,
-        DAYNAME(STR_TO_DATE(date, '%d/%m/%Y')) AS day_name,
-        COUNT(*) AS no_transactions,
-        RANK() OVER(PARTITION BY branch ORDER BY COUNT(*) DESC) AS rank
-    FROM walmart
-    GROUP BY branch, day_name
-) AS ranked
-WHERE rank = 1;
-
--- Q4: Calculate the total quantity of items sold per payment method
-SELECT 
-    payment_method,
-    SUM(quantity) AS no_qty_sold
-FROM walmart
-GROUP BY payment_method;
-
--- Q5: Determine the average, minimum, and maximum rating of categories for each city
-SELECT 
-    city,
-    category,
-    MIN(rating) AS min_rating,
-    MAX(rating) AS max_rating,
-    AVG(rating) AS avg_rating
-FROM walmart
-GROUP BY city, category;
-
--- Q6: Calculate the total profit for each category
-SELECT 
-    category,
-    SUM(unit_price * quantity * profit_margin) AS total_profit
-FROM walmart
-GROUP BY category
-ORDER BY total_profit DESC;
-
--- Q7: Determine the most common payment method for each branch
-WITH cte AS (
-    SELECT 
-        branch,
-        payment_method,
-        COUNT(*) AS total_trans,
-        RANK() OVER(PARTITION BY branch ORDER BY COUNT(*) DESC) AS rank
-    FROM walmart
-    GROUP BY branch, payment_method
-)
-SELECT branch, payment_method AS preferred_payment_method
-FROM cte
-WHERE rank = 1;
-
--- Q8: Categorize sales into Morning, Afternoon, and Evening shifts
-SELECT
-    branch,
-    CASE 
-        WHEN HOUR(TIME(time)) < 12 THEN 'Morning'
-        WHEN HOUR(TIME(time)) BETWEEN 12 AND 17 THEN 'Afternoon'
-        ELSE 'Evening'
-    END AS shift,
-    COUNT(*) AS num_invoices
-FROM walmart
-GROUP BY branch, shift
-ORDER BY branch, num_invoices DESC;
-
--- Q9: Identify the 5 branches with the highest revenue decrease ratio from last year to current year (e.g., 2022 to 2023)
-WITH revenue_2022 AS (
-    SELECT 
-        branch,
-        SUM(total) AS revenue
-    FROM walmart
-    WHERE YEAR(STR_TO_DATE(date, '%d/%m/%Y')) = 2022
-    GROUP BY branch
+        type,
+        rating,
+        COUNT(*) AS rating_count
+    FROM netflix
+    GROUP BY type, rating
 ),
-revenue_2023 AS (
+RankedRatings AS (
     SELECT 
-        branch,
-        SUM(total) AS revenue
-    FROM walmart
-    WHERE YEAR(STR_TO_DATE(date, '%d/%m/%Y')) = 2023
-    GROUP BY branch
+        type,
+        rating,
+        rating_count,
+        RANK() OVER (PARTITION BY type ORDER BY rating_count DESC) AS rank
+    FROM RatingCounts
 )
 SELECT 
-    r2022.branch,
-    r2022.revenue AS last_year_revenue,
-    r2023.revenue AS current_year_revenue,
-    ROUND(((r2022.revenue - r2023.revenue) / r2022.revenue) * 100, 2) AS revenue_decrease_ratio
-FROM revenue_2022 AS r2022
-JOIN revenue_2023 AS r2023 ON r2022.branch = r2023.branch
-WHERE r2022.revenue > r2023.revenue
-ORDER BY revenue_decrease_ratio DESC
-LIMIT 5;
+    type,
+    rating AS most_frequent_rating
+FROM RankedRatings
+WHERE rank = 1;
+
+
+-- 3. List all movies released in a specific year (e.g., 2020)
+
+SELECT * 
+FROM netflix
+WHERE release_year = 2020
+
+
+-- 4. Find the top 5 countries with the most content on Netflix
+
+SELECT * 
+FROM
+(
+	SELECT 
+		-- country,
+		UNNEST(STRING_TO_ARRAY(country, ',')) as country,
+		COUNT(*) as total_content
+	FROM netflix
+	GROUP BY 1
+)as t1
+WHERE country IS NOT NULL
+ORDER BY total_content DESC
+LIMIT 5
+
+
+-- 5. Identify the longest movie
+
+SELECT 
+	*
+FROM netflix
+WHERE type = 'Movie'
+ORDER BY SPLIT_PART(duration, ' ', 1)::INT DESC
+
+
+-- 6. Find content added in the last 5 years
+SELECT
+*
+FROM netflix
+WHERE TO_DATE(date_added, 'Month DD, YYYY') >= CURRENT_DATE - INTERVAL '5 years'
+
+
+-- 7. Find all the movies/TV shows by director 'Rajiv Chilaka'!
+
+SELECT *
+FROM
+(
+
+SELECT 
+	*,
+	UNNEST(STRING_TO_ARRAY(director, ',')) as director_name
+FROM 
+netflix
+)
+WHERE 
+	director_name = 'Rajiv Chilaka'
+
+
+
+-- 8. List all TV shows with more than 5 seasons
+
+SELECT *
+FROM netflix
+WHERE 
+	TYPE = 'TV Show'
+	AND
+	SPLIT_PART(duration, ' ', 1)::INT > 5
+
+
+-- 9. Count the number of content items in each genre
+
+SELECT 
+	UNNEST(STRING_TO_ARRAY(listed_in, ',')) as genre,
+	COUNT(*) as total_content
+FROM netflix
+GROUP BY 1
+
+
+-- 10. Find each year and the average numbers of content release by India on netflix. 
+-- return top 5 year with highest avg content release !
+
+
+SELECT 
+	country,
+	release_year,
+	COUNT(show_id) as total_release,
+	ROUND(
+		COUNT(show_id)::numeric/
+								(SELECT COUNT(show_id) FROM netflix WHERE country = 'India')::numeric * 100 
+		,2
+		)
+		as avg_release
+FROM netflix
+WHERE country = 'India' 
+GROUP BY country, 2
+ORDER BY avg_release DESC 
+LIMIT 5
+
+
+-- 11. List all movies that are documentaries
+SELECT * FROM netflix
+WHERE listed_in LIKE '%Documentaries'
+
+
+
+-- 12. Find all content without a director
+SELECT * FROM netflix
+WHERE director IS NULL
+
+
+-- 13. Find how many movies actor 'Salman Khan' appeared in last 10 years!
+
+SELECT * FROM netflix
+WHERE 
+	casts LIKE '%Salman Khan%'
+	AND 
+	release_year > EXTRACT(YEAR FROM CURRENT_DATE) - 10
+
+
+-- 14. Find the top 10 actors who have appeared in the highest number of movies produced in India.
+
+
+
+SELECT 
+	UNNEST(STRING_TO_ARRAY(casts, ',')) as actor,
+	COUNT(*)
+FROM netflix
+WHERE country = 'India'
+GROUP BY 1
+ORDER BY 2 DESC
+LIMIT 10
+
+/*
+Question 15:
+Categorize the content based on the presence of the keywords 'kill' and 'violence' in 
+the description field. Label content containing these keywords as 'Bad' and all other 
+content as 'Good'. Count how many items fall into each category.
+*/
+
+
+SELECT 
+    category,
+	TYPE,
+    COUNT(*) AS content_count
+FROM (
+    SELECT 
+		*,
+        CASE 
+            WHEN description ILIKE '%kill%' OR description ILIKE '%violence%' THEN 'Bad'
+            ELSE 'Good'
+        END AS category
+    FROM netflix
+) AS categorized_content
+GROUP BY 1,2
+ORDER BY 2
+
+
+
+
+-- End of reports
